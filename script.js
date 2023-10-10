0b263cf92a8e86b010cd6cfc4cccb7f970990f68
alok3000times <145034346+alok3000times@users.noreply.github.com>
2023-10-10 17:01:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const apiKey="cd9343cb82f9b512fc6f93c00313334a";
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox=document.querySelector(".search input");
+const searchBtn=document.querySelector(".search button");
+const weatherIcon=document.querySelector(".weather-icon");
+async function checkWeather(city){
+    const response=await fetch(apiUrl+city+`&appid=${apiKey}`);
+    var data=await  response.json();
+    console.log(data);
+    document.querySelector(".city").innerHTML=data.name;
+    document.querySelector(".temp").innerHTML=Math.round(data.main.temp)+"°c";
+    document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
+    document.querySelector(".wind").innerHTML=data.wind.speed+"km/h";
+ if(data.weather[0].main == "Clouds"){
+    weatherIcon.src="clouds.png";
+ }
+ else if(data.weather[0].main == "Clear"){
+    weatherIcon.src="clear.png";
+}
+else if(data.weather[0].main == "Rain"){
+    weatherIcon.src="rain.png";
+}
+else if(data.weather[0].main == "Drizzle"){
+    weatherIcon.src="drizzle.png";
+}
+else if(data.weather[0].main == "Mist"){
+        weatherIcon.src="mist.png";
+}
+}
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+})
